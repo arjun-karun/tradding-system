26470742c10cec480c4531b753b297922812b53c
Arjun Karun <arjunkarunr@gmail.com>
2018-10-06 18:29:11 +0530

    Added Erc-20

--- contracts/SellFactory.sol
+++ contracts/SellFactory.sol
@@ -1,6 +1,12 @@
 pragma solidity ^0.4.24;
 
-contract SellFactory {
+
+import "./erc20.sol";
+import "./safemath.sol";
+
+contract SellFactory is ERC20_Token {
+
+  using SafeMath for uint256;  
 
   struct  Sale {
       string saleType;
@@ -8,16 +14,19 @@ contract SellFactory {
       string location;
       string expetedDeliveryDate;
   }
-
+  
   Sale[] public sales;
   mapping (uint => address) public saleToOwner;
+  mapping (address => uint) public ownerToUnits;
   mapping (address => uint) ownerSaleCount;
+  mapping (address => mapping (address => uint256)) public biddingApproval;
 
 
   function createSale(string _saleType, uint _units, string _location, string _expectedTime) public {
     uint saleId = sales.push(Sale(_saleType, _units,  _location, _expectedTime)) - 1;
     saleToOwner[saleId] = msg.sender;
-    ownerSaleCount[msg.sender]++;
+    ownerSaleCount[msg.sender].add(1);
+    ownerToUnits[msg.sender].add(_units);
   }
 
   function getSalesCount() public constant returns(uint) {
@@ -30,4 +39,91 @@ contract SellFactory {
       sales[_indexId].location, 
       sales[_indexId].expetedDeliveryDate);
   }
+    /**
+     * Prevent an account from behing 0x0
+     * @param addr Address to check
+     */
+    modifier No0x(address addr) { 
+        if (addr == 0x0) revert();
+        _; 
+    }
+
+    /**
+     * A modifer to check validity of a balance for a transfer
+     * from an account to another.
+     * @param from  [description]
+     * @param to    [description]
+     * @param value [description]
+     */
+    modifier ValidBalance(address from, address to, uint256 value) { 
+        if (ownerToUnits[from] < value) revert();                 // Check if the sender has enough
+        if (ownerToUnits[to] + value < ownerToUnits[to]) revert();  // Check for overflows
+        _; 
+    }
+
+    /**
+     * Returns the total amount of tokens
+     * @return total amount
+     */
+    function totalSupply() public constant returns(uint256 _totalSupply) {
+        return ownerToUnits[msg.sender];
+    }
+
+    /**
+     * Returns The balance of a given account
+     * @param addr Address of the account
+     * @return Balance
+     */
+    function balanceOf(address addr) public constant returns(uint256 balance) {
+        return ownerToUnits[addr];
+    }
+    
+    /**
+     * Returns the amount which _spender is still allowed to withdraw from _owner
+     */
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+        return ownerToUnits[_owner];    
+    }
+
+    /**
+     * Send coins
+     * @param _to        The recipient of tokens
+     * @param _value     Amount of tokens to send 
+     */
+     function transfer(address _to, uint256 _value) public No0x(_to) ValidBalance(msg.sender, _to, _value) 
+     returns (bool success) {                        
+        ownerToUnits[msg.sender].sub(_value);                      // Subtract from the sender
+        ownerToUnits[_to].add(_value);                             // Add the same to the recipient
+        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+        return true;
+    }
+
+    /**
+     * Allow another contract to spend some tokens in your behalf
+     * @param _spender     Account that can take some of your tokens
+     * @param _value       Max amount of tokens the _spender account can take
+     * @return {return}    Return true if the action succeeded
+     */
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }  
+
+    /**
+     * A contract attempts to get the coins
+     * @param _from     Address holding the tokens to transfer
+     * @param _to       Account to send the coins to
+     * @param _value    How many tokens     
+     * @return {bool}   Whether the call was successful
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public No0x (_to) ValidBalance(_from, _to, _value)
+    returns (bool success) {
+        if (_value > allowance[_from][msg.sender]) revert();     // Check allowance
+        balances[_from] -= _value;                               // Subtract from the sender
+        balances[_to] += _value;                                 // Add the same to the recipient
+        allowance[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
+        return true;
+    }
 }
--- contracts/erc20.sol
+++ contracts/erc20.sol
@@ -1,27 +1,47 @@
+/**
+ * See https://github.com/ethereum/EIPs/issues/200
+ */
+contract ERC20_Token {
+    // Triggered when tokens are transferred.
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-/// erc20.sol -- API for the ERC20 token standard
+    // Triggered whenever approve(address _spender, uint256 _value) is called.
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-// See <https://github.com/ethereum/EIPs/issues/20>.
-
-// This file likely does not meet the threshold of originality
-// required for copyright to apply.  As a result, this is free and
-// unencumbered software belonging to the public domain.
-
-pragma solidity ^0.4.8;
-
-contract ERC20Events {
-    event Approval(address indexed src, address indexed guy, uint wad);
-    event Transfer(address indexed src, address indexed dst, uint wad);
+    // Get the total token supply
+    function totalSupply() public constant returns (uint256 _totalSupply);
+    
+    // Get the account balance of another account with address _owner
+    function balanceOf(address _owner) public constant returns (uint256 balance);
+    
+    // Send _value amount of tokens to address _to
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    
+    // Send _value amount of tokens from address _from to address _to
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    
+    // Allow _spender to withdraw from your account, multiple times, up to the _value amount. If this function is called again it overwrites the current allowance with _value.
+    function approve(address _spender, uint256 _value) public returns (bool success);
+    
+    // Returns the amount which _spender is still allowed to withdraw from _owner
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
 }
 
-contract ERC20 is ERC20Events {
-    function totalSupply() public view returns (uint);
-    function balanceOf(address guy) public view returns (uint);
-    function allowance(address src, address guy) public view returns (uint);
+contract ERC20_Details {
+    string tokenName;
+    string tokenSymbol;
+    uint8 tokenDecimals;
 
-    function approve(address guy, uint wad) public returns (bool);
-    function transfer(address dst, uint wad) public returns (bool);
-    function transferFrom(
-        address src, address dst, uint wad
-    ) public returns (bool);
+    function name() public view returns (string _name);
+    function symbol() public view returns (string _symbol);
+    function decimals() public view returns (uint8 _decimals);
+
+    function ERC20_Details(string _name, string _symbol, uint8 _decimals) public {
+        require (keccak256(_name) != keccak256(''));
+        require (keccak256(_symbol) != keccak256(''));
+
+        tokenName = _name;
+        tokenSymbol = _symbol;
+        tokenDecimals = _decimals;
+    }
 }
